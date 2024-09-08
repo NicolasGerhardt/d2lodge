5024e473fe47d38757cc406361d2072212124262
Nic Gerhardt <Nicolas.Gerhardt@gmail.com>
2024-09-08 17:08:36 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1>Hello brothers of Detroit #2</h1>
+            <p>Our payment portal is currently down.</p>
+            <p>The Grand Lodge of Michigan is switching over between online payment processors and as such we are unable to take online payments at this time.</p>
+            <p>Once we are able to take online dues payments this site will be updated to redirect to the new processor.</p>
+            <p>Thank you for your understanding, please direct any questions and payments to secretary Pat at <a href="mailto:detroit2pm2016@gmail.com">detroit2pm2016@gmail.com</a></p>
+        </>
+    )
+}
+
+export default App
